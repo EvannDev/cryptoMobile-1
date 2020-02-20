59f0c3618981876005f285acd8cbedff1fde34af
Allan <allan.duee@isen.yncrea.fr>
2020-02-20 22:40:40 +0100

    Enable Click on Patient in PatientsInfoActivity. A Click on one of the patient will display a Toaster with his first name, last name and his title

--- SecuGerbisNucleaires/app/src/main/java/fr/isen/gerbisnucleaires/secugerbisnucleaires/PatientsInfoActivity.kt
+++ SecuGerbisNucleaires/app/src/main/java/fr/isen/gerbisnucleaires/secugerbisnucleaires/PatientsInfoActivity.kt
@@ -1,28 +1,37 @@
 package fr.isen.gerbisnucleaires.secugerbisnucleaires
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.LinearLayout
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import fr.isen.gerbisnucleaires.secugerbisnucleaires.recyclerview.PatientAdapter
 import fr.isen.gerbisnucleaires.secugerbisnucleaires.recyclerview.patient.Name
 import fr.isen.gerbisnucleaires.secugerbisnucleaires.recyclerview.patient.Patient
 import kotlinx.android.synthetic.main.activity_patients_info.*
 
-class PatientsInfoActivity : AppCompatActivity() {
+class PatientsInfoActivity : AppCompatActivity(), PatientAdapter.OnItemClickListener {
+    override fun onItemClick(patient: Patient) {
+        Toast.makeText(this, "Clicked: ${patient.name}", Toast.LENGTH_LONG).show()
+    }
 
     private val patients: ArrayList<Patient> = arrayListOf(
         Patient(Name("Marsaut", "Mayeul", "Mr"), "Fou", 21),
         Patient(Name("Duee", "Allan", "Mr"), "Autorigole", 22),
         Patient(Name("Thomas", "Valentin", "Mme"), "Existe", 45),
-        Patient(Name("Garcia Rota", "Lucas", "Mr"), "Monocouille et tri tétons",8)
+        Patient(Name("Garcia Rota", "Lucas", "Mr"), "Monocouille et tri tétons",8) ,
+        Patient(Name("Bilisari", "Elio", "Mr"), "Test",100),
+        Patient(Name("De Bailliencourt", "Evann", "Mr"), "Test 1234 ",32)
     )
 
+    @SuppressLint("WrongConstant")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_patients_info)
 
         patientsRecycler.layoutManager = LinearLayoutManager(this)
-        patientsRecycler.adapter = PatientAdapter(patients)
+        patientsRecycler.adapter = PatientAdapter(patients,this)
     }
 }
--- SecuGerbisNucleaires/app/src/main/java/fr/isen/gerbisnucleaires/secugerbisnucleaires/recyclerview/PatientHolder.kt
+++ SecuGerbisNucleaires/app/src/main/java/fr/isen/gerbisnucleaires/secugerbisnucleaires/recyclerview/PatientHolder.kt
@@ -6,10 +6,14 @@ import fr.isen.gerbisnucleaires.secugerbisnucleaires.recyclerview.patient.Patien
 import kotlinx.android.synthetic.main.activity_patient_item.view.*
 
 class PatientHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-    fun bindValue(patient: Patient) {
+    fun bind(patient: Patient, clickListener : PatientAdapter.OnItemClickListener) {
         itemView.patientTitle.text = patient.name.title
         itemView.patientName.text = (patient.name.name + " " + patient.name.firstName)
         itemView.patientAge.text =  patient.age.toString()
         itemView.patientDisease.text = patient.disease
+
+        itemView.setOnClickListener {
+            clickListener.onItemClick(patient)
+        }
     }
 }
