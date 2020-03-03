afe078498375da3ee88856157d0280103f21b0b7
Lucas GARCIA--ROTA <lucas.garcia-rota@isen.yncrea.fr>
2020-03-03 20:02:45 +0100

    SignUp remarche

--- SecuGerbisNucleaires/app/src/main/java/fr/isen/gerbisnucleaires/secugerbisnucleaires/SignUpActivity.kt
+++ SecuGerbisNucleaires/app/src/main/java/fr/isen/gerbisnucleaires/secugerbisnucleaires/SignUpActivity.kt
@@ -6,22 +6,12 @@ import android.util.Patterns
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.FirebaseUser
-import com.google.firebase.database.*
 import kotlinx.android.synthetic.main.activity_sign_up.*
 
 
 class SignUpActivity : AppCompatActivity() {
 
-    val email = emailSignUpEdit.text.toString()
-    val password = passwordSignUpEdit.text.toString()
-    val firstname = firstnameSignUpEdit.text.toString()
-    val lastname = lastnameSignUpEdit.text.toString()
-    val phone = phoneSignUpEdit.text.toString()
-    val adminCode = codeAdminEdit.text.toString()
-
     private lateinit var mAuth: FirebaseAuth
-    private lateinit var userDatabase: DatabaseReference
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,28 +19,18 @@ class SignUpActivity : AppCompatActivity() {
 
         mAuth = FirebaseAuth.getInstance()
 
-        userDatabase = FirebaseDatabase.getInstance().reference
-
         buttonsignup.setOnClickListener{
             registerUser()
         }
     }
 
     private fun registerUser() {
-
-        val database = FirebaseDatabase.getInstance()
-        var ref = database.getReference("Code_Admin")
-
-
-        ref.addValueEventListener(object : ValueEventListener {
-            override fun onDataChange(dataSnapshot: DataSnapshot) {
-                val realAdminCodeDB = dataSnapshot.getValue()!!
-            }
-
-            override fun onCancelled(databaseError: DatabaseError) {
-                println("The read failed: " + databaseError.code)
-            }
-        })
+        val email = emailSignUpEdit.text.toString()
+        val password = passwordSignUpEdit.text.toString()
+        val firstname = firstnameSignUpEdit.text.toString()
+        val lastname = lastnameSignUpEdit.text.toString()
+        val phone = phoneSignUpEdit.text.toString()
+        val adminCode = codeAdminEdit.text.toString()
 
 
         if( email.isEmpty()     ||
@@ -79,46 +59,22 @@ class SignUpActivity : AppCompatActivity() {
         }
     }
 
-    override fun onStart() {
-        super.onStart()
-        // Check if user is signed in (non-null) and update UI accordingly.
-        val currentUser = mAuth.currentUser
-        updateUI(currentUser)
-    }
-
-
     private fun createAccount(email: String, password: String) {
-
-        var map = mutableMapOf<String, Any>()
-        map["firstname"] = firstname
-        map["lastname"] = lastname
-        map["phone"] = phone
-        map["email"] = email
-        map["password"] = password
-
-        val userID = FirebaseDatabase.getInstance().reference.child("Nurse").push().key.toString()
-
-        FirebaseDatabase.getInstance().reference.child("Nurse").child(userID).setValue(map).addOnCompleteListener {
-            Toast.makeText(this, "Registered", Toast.LENGTH_LONG).show()
-        }
 
         mAuth.createUserWithEmailAndPassword(email, password)
             .addOnCompleteListener(this) { task ->
                 if (task.isSuccessful) {
                     val user = mAuth.currentUser
                     sendEmailVerification()
-                    updateUI(user)
                     goToLogin()
                 } else {
                     Toast.makeText(baseContext, "Authentication failed.",
                         Toast.LENGTH_SHORT).show()
-                    updateUI(null)
                 }
             }
     }
 
     private fun sendEmailVerification(){
-
         val user = mAuth.currentUser
         user?.sendEmailVerification()
             ?.addOnCompleteListener(this) { task ->
@@ -135,14 +91,6 @@ class SignUpActivity : AppCompatActivity() {
             }
     }
 
-    private fun updateUI(user: FirebaseUser?) {
-        if(user != null){
-            Toast.makeText(this,"You already have an account",Toast.LENGTH_LONG).show();
-        }else {
-            Toast.makeText(this,"You don't have account",Toast.LENGTH_LONG).show();
-        }
-    }
-
     private fun goToLogin() {
         val homeIntent = Intent(
             this,
@@ -150,4 +98,7 @@ class SignUpActivity : AppCompatActivity() {
         )
         startActivity(homeIntent)
     }
+
+
+
 }
