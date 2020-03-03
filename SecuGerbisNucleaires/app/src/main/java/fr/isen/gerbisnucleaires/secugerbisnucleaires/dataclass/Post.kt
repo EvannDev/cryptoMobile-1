a3c43d93150becfb8c0aa02c76bb9b78cb2a1d16
Evann <EvannDev@users.noreply.github.com>
2020-03-03 11:39:02 +0100

    update login

---
+++ SecuGerbisNucleaires/app/src/main/java/fr/isen/gerbisnucleaires/secugerbisnucleaires/dataclass/Post.kt
@@ -0,0 +1,24 @@
+package fr.isen.gerbisnucleaires.secugerbisnucleaires.dataclass
+
+data class Post(
+    var uid: String,
+    var firstname: String,
+    var lastname: String,
+    var email: String,
+    var phone: String,
+    var starCount: Int = 0,
+    var stars: MutableMap<String, Boolean> = HashMap()
+){
+    fun toMap(): Map<String, Any?>{
+        return mapOf(
+            "uid" to uid,
+            "firstname" to firstname,
+            "lastname" to lastname,
+            "email" to email,
+            "phone" to phone,
+            "starCount" to starCount,
+            "stars" to stars
+
+        )
+    }
+}
