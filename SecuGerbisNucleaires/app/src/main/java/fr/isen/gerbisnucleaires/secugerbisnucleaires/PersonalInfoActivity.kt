9a6d955ac6e42cf6b97b8fb8bd094fcca8e6db81
Lucas GARCIA--ROTA <lucas.garcia-rota@isen.yncrea.fr>
2020-03-03 14:26:46 +0100

    Coorection de bug

--- SecuGerbisNucleaires/app/src/main/java/fr/isen/gerbisnucleaires/secugerbisnucleaires/PersonalInfoActivity.kt
+++ SecuGerbisNucleaires/app/src/main/java/fr/isen/gerbisnucleaires/secugerbisnucleaires/PersonalInfoActivity.kt
@@ -5,7 +5,10 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
-import com.google.firebase.database.*
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 import kotlinx.android.synthetic.main.activity_personal_item.*
 
 class PersonalInfoActivity : AppCompatActivity() {
@@ -17,7 +20,7 @@ class PersonalInfoActivity : AppCompatActivity() {
         FirebaseDatabase.getInstance().reference
             .child("Nurse")
             .child("Mettre_Un_Nurse_Id_en_lien_avec_Evann_a_la_connexion")
-            .addListenerForSingleValueEvent(object : ValueEventListener{
+            .addListenerForSingleValueEvent(object : ValueEventListener {
                 override fun onCancelled(p0: DatabaseError) {
                     Toast.makeText(applicationContext, "Can not read data from database", Toast.LENGTH_LONG).show()
                 }
