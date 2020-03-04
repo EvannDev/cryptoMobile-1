b23cddd63ce68390404946e77ee05b72296716be
Evann <52197794+EvannDev@users.noreply.github.com>
2020-03-04 16:13:03 +0100

    Update Nurse.kt

--- SecuGerbisNucleaires/app/src/main/java/fr/isen/gerbisnucleaires/secugerbisnucleaires/dataclass/Nurse.kt
+++ SecuGerbisNucleaires/app/src/main/java/fr/isen/gerbisnucleaires/secugerbisnucleaires/dataclass/Nurse.kt
@@ -1,3 +1,3 @@
 package fr.isen.gerbisnucleaires.secugerbisnucleaires.dataclass
 
-data class Nurse(val id: String, val firstname:String, val lastname: String, val phone: String, val email:String)
+data class Nurse(val id: String, val firstname:String, val lastname: String, val phone: String, val email:String, val password:String)
