7d87ed767aa4b2de0025c9b800a6f2e876aab35c
Evann <52197794+EvannDev@users.noreply.github.com>
2020-03-04 16:24:39 +0100

    Update PersonalInfoActivity.kt

--- SecuGerbisNucleaires/app/src/main/java/fr/isen/gerbisnucleaires/secugerbisnucleaires/PersonalInfoActivity.kt
+++ SecuGerbisNucleaires/app/src/main/java/fr/isen/gerbisnucleaires/secugerbisnucleaires/PersonalInfoActivity.kt
@@ -5,10 +5,13 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
+import kotlinx.android.synthetic.main.activity_edit_personal.*
+import com.google.firebase.database.*
 import kotlinx.android.synthetic.main.activity_personal_item.*
 
 class PersonalInfoActivity : AppCompatActivity() {
@@ -17,30 +20,71 @@ class PersonalInfoActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_personal_item)
 
+        val user = FirebaseAuth.getInstance().currentUser
+
         FirebaseDatabase.getInstance().reference
             .child("Nurse")
-            .child("Mettre_Un_Nurse_Id_en_lien_avec_Evann_a_la_connexion")
+            .child(user!!.uid)
             .addListenerForSingleValueEvent(object : ValueEventListener {
+        val ref = FirebaseDatabase.getInstance()
+        val user = FirebaseAuth.getInstance().currentUser
+
+        val dataOfflineRef = FirebaseDatabase.getInstance().getReference("Nurse/")
+        dataOfflineRef.keepSynced(true)
+
+        //Detecting Connection State
+        val connectedRef = FirebaseDatabase.getInstance().getReference(".info/connected")
+        connectedRef.addValueEventListener(object : ValueEventListener{
+            override fun onCancelled(p0: DatabaseError) {
+                Toast.makeText(this@PersonalInfoActivity,"onCancelled",Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onDataChange(p0: DataSnapshot) {
+               val connected = p0.getValue(Boolean::class.java) ?: false
+                if(connected){
+                    Toast.makeText(this@PersonalInfoActivity,"Online",Toast.LENGTH_LONG).show()
+                }
+                else{
+                    Toast.makeText(this@PersonalInfoActivity,"Offline",Toast.LENGTH_LONG).show()
+                }
+            }
+        })
+
+
+        ref.reference
+            .child("Nurse")
+
+            .child(user!!.uid)
+            .addListenerForSingleValueEvent(object : ValueEventListener{
                 override fun onCancelled(p0: DatabaseError) {
-                    Toast.makeText(applicationContext, "Can not read data from database", Toast.LENGTH_LONG).show()
+                    Toast.makeText(applicationContext, "Can't read data from database", Toast.LENGTH_LONG).show()
                 }
 
                 override fun onDataChange(p0: DataSnapshot) {
-                    var map = p0.value as Map<String, Any>
+                    val map = p0.value as Map<String, Any>
                     firstnameNurse.text = map["firstname"].toString()
                     lastnameNurse.text = map["lastname"].toString()
                     phoneNurse.text = map["phone"].toString()
                     emailNurse.text = map["email"].toString()
+                    editButtonClick(map["firstname"].toString(), map["lastname"].toString(), map["phone"].toString(), map["email"].toString())
                 }
             })
 
-
-        buttonEdit.setOnClickListener {
-            newIntent(this, EditPersonalActivity::class.java)
+        homeButton.setOnClickListener{
+            newIntent(this, HomeActivity::class.java)
+            this.finish()
         }
     }
 
-    private fun newIntent(context: Context, clazz: Class<*>) {
-        startActivity(Intent(context, clazz))
+    fun editButtonClick(firstname: String, lastname: String, phone: String, email: String) {
+        buttonEdit.setOnClickListener {
+            val intent = Intent(this, EditPersonalActivity::class.java)
+            intent.putExtra("firstname", firstname)
+            intent.putExtra("lastname", lastname)
+            intent.putExtra("phone", phone)
+            intent.putExtra("email", email)
+            startActivity(intent)
+            this.finish()
+        }
     }
 }
